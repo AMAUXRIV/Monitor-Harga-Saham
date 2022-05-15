8234bf1a959d62a403c347f49b17e5db9311d1ca
AMAUXRIV <pmaulana981@gmail.com>
2022-05-15 11:46:08 +0700

    dragonnewbie1

---
+++ basic.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import yfinance as fy
+import plotly.graph_objects as go
+import util
+
+if __name__ == '__main__':
+    ticker_symbol = st.sidebar.text_input(
+        "Please enter the stock symbol", "MSFT"
+        )
+    data_period = st.sidebar.text_input('Period','10d')
+    data_interval = st.sidebar.radio('Interval',['15m','30m','1h','1d','5d'])
+
+
+    st.header(ticker_symbol)
+    ticker_data=util.get_ticker_data(ticker_symbol,data_period,data_interval)
+
+    ticker_data
+
+    util.plot_candle_chart(ticker_data)
---
+++ util.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import plotly.graph_objects as go
+import plotly.express as px
+import yfinance as yf
+
+
+def get_ticker_data(ticker_symbol,data_period, data_interval):
+    ticker_data =yf.download(tickers=ticker_symbol,period=data_period,interval=data_interval)
+    if len(ticker_data) == 0 :
+        st.write('Could not find the ticker data. Modify ticker symbol or reduce the period value')
+    else:
+        #format the x-axis to skip dates with missing values
+        ticker_data.index = ticker_data.index.strftime("%d-%m-%Y %H:%M")
+        
+    return ticker_data
+
+def plot_candle_chart(ticker_data):
+    candle_fig = go.Figure()
+    candle_fig.add_trace(go.Candlestick(x=ticker_data.index,
+                                 open=ticker_data['Open'],
+                                 high=ticker_data['High'],
+                                 low=ticker_data['Low'],
+                                 close=ticker_data['Close'],
+                                 name='Amau Market Data'))
+    candle_fig.update_layout(height=800)
+    st.write(candle_fig)
+    
